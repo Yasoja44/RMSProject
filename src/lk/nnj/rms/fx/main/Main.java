279ef0d151db0e6e10209523b99c256e0aa14894
NNJ16 <nethsera@gmail.com>
2020-08-05 13:23:17 +0530

    Change project name in Main Class by NNJ16

--- src/lk/nnj/rms/fx/main/Main.java
+++ src/lk/nnj/rms/fx/main/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/lk/nnj/rms/fx/view/PlaceOrderForm.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Restaurant Management System");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
